7449767a00166989659f7e5807468297d3ae8d22
Selena <selena.k.rodriguez@gmail.com>
2023-08-16 14:46:29 -0500

    added skeleton components

--- cv-application/src/components/ResumeDetails.jsx
+++ cv-application/src/components/ResumeDetails.jsx
@@ -1,13 +1,29 @@
+import EducationDetails from "./resume-display/EducationDetails";
+import ExperienceDetails from "./resume-display/ExperienceDetails";
+import PersonalDetails from "./resume-display/PersonalDetails";
+import SkillDetails from "./resume-display/SkillsDetails";
+import SummaryDetails from "./resume-display/SummaryDetails";
+
 function ResumeDetails() {
-    return (
-        <div id="resumeDetailCategories">
-            {/* <PersonalDisplay />
-            <SummaryDisplay />
-            <SkillDisplay />
-            <ExperienceDisplay />
-            <EducationDisplay /> */}
-        </div>
-    )
+  return (
+    <>
+      <section id="personalDetails">
+        <PersonalDetails />
+      </section>
+      <section id="summaryDetails">
+        <SummaryDetails />
+      </section>
+      <section id="skillDetails">
+        <SkillDetails />
+      </section>
+      <section id="experienceDetails">
+        <ExperienceDetails />
+      </section>
+      <section id="educationDetails">
+        <EducationDetails />
+      </section>
+    </>
+  );
 }
 
 export default ResumeDetails;
