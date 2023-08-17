53624a1735a0c8b0abf425808c73bbaddc7033cc
Selena <selena.k.rodriguez@gmail.com>
2023-08-16 21:31:45 -0500

    Added functionality for the full name and summary to be updated when the save button is clicked

--- cv-application/src/components/resume-display/PersonalDetails.jsx
+++ cv-application/src/components/resume-display/PersonalDetails.jsx
@@ -1,10 +1,23 @@
-function PersonalDetails() {
+/* eslint-disable react/prop-types */
+import { useEffect, useState } from "react";
+
+function PersonalDetails({ fullName }) {
+  const [placeholder, setPlaceholder] = useState("Full Name");
+
+  useEffect(() => {
+    if (fullName) {
+      setPlaceholder(fullName);
+    } else {
+      setPlaceholder("Full Name");
+    }
+  }, [fullName]);
+
   return (
     <>
-        <h2>Full Name</h2>
-        <h3>email</h3>
-        <h3>phone</h3>
-        <h3>address</h3>
+      <h2>{placeholder}</h2>
+      <h3>email</h3>
+      <h3>phone</h3>
+      <h3>address</h3>
     </>
   );
 }
--- cv-application/src/components/resume-display/SummaryDetails.jsx
+++ cv-application/src/components/resume-display/SummaryDetails.jsx
@@ -1,9 +1,21 @@
+import { useEffect, useState } from "react";
+
 /* eslint-disable react/prop-types */
 function SummaryDetails({ summary }) {
+  const [placeholder, setPlaceholder] = useState("Insert text");
+
+  useEffect(() => {
+    if (summary) {
+      setPlaceholder(summary);
+    } else {
+      setPlaceholder("Insert Text");
+    }
+  }, [summary]);
+
   return (
     <>
       <h2>Summary</h2>
-      <h3>{summary}</h3>
+      <h3>{placeholder}</h3>
     </>
   );
 }
