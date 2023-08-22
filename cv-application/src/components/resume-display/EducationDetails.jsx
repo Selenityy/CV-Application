9edf520a6e13cd349f210071ed40974fed5097cf
Selena <selena.k.rodriguez@gmail.com>
2023-08-22 11:18:06 -0500

    Added placeholder text

--- cv-application/src/components/resume-display/EducationDetails.jsx
+++ cv-application/src/components/resume-display/EducationDetails.jsx
@@ -1,16 +1,43 @@
-/* eslint-disable react/prop-types */
+import { useEffect, useState } from "react";
 
+/* eslint-disable react/prop-types */
 function EducationDetails({ schoolList }) {
+  const [placeholder, setPlaceholder] = useState([
+    {
+      id: "placeholder",
+      name: "Insert school name",
+      degree: "Insert degree",
+      start: "Insert start date",
+      end: "Insert end date",
+    },
+  ]);
+
+  useEffect(() => {
+    if (schoolList.length > 0) {
+      setPlaceholder(schoolList);
+    } else {
+      setPlaceholder([
+        {
+          id: "placeholder",
+          name: "Insert school name",
+          degree: "Insert degree",
+          start: "Insert start date",
+          end: "Insert end date",
+        },
+      ]);
+    }
+  }, [schoolList]);
+
   return (
     <>
       <h2>Education</h2>
       <ul>
-        {schoolList.map((schoolItem) => (
+        {placeholder.map((schoolItem) => (
           <li key={schoolItem.id}>
-            <p>School Name: {schoolItem.name}</p>
-            <p>Degree: {schoolItem.degree}</p>
-            <p>Start Date: {schoolItem.start}</p>
-            <p>End Date: {schoolItem.end}</p>
+            <p>{schoolItem.name}</p>
+            <p>{schoolItem.degree}</p>
+            <p>{schoolItem.start}</p>
+            <p>{schoolItem.end}</p>
           </li>
         ))}
       </ul>
--- cv-application/src/components/resume-display/SkillsDetails.jsx
+++ cv-application/src/components/resume-display/SkillsDetails.jsx
@@ -1,12 +1,29 @@
-/* eslint-disable react/prop-types */
+import { useEffect, useState, useMemo } from "react";
 
+/* eslint-disable react/prop-types */
 function SkillDetails({ skillList }) {
+  const placeholderSkill = useMemo(
+    () => ({ id: "placeholder", skill: "Insert skill" }),
+    []
+  );
+  const [placeholder, setPlaceholder] = useState([placeholderSkill]);
+
+  useEffect(() => {
+    if (skillList.length > 0) {
+      setPlaceholder(skillList);
+    } else {
+      setPlaceholder([placeholderSkill]);
+    }
+  }, [skillList, placeholderSkill]);
+
   return (
     <>
       <h2>Skills</h2>
       <ul>
-        {skillList.map((skillItem) => (
-          <li key={skillItem.id}>{skillItem.skill}</li>
+        {placeholder.map((skillItem) => (
+          <li key={skillItem.id}>
+            <p>{skillItem.skill}</p>
+          </li>
         ))}
       </ul>
     </>
