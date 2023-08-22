d6663fa8c0d049e7441454e972b33d94f67a3aa3
Selena <selena.k.rodriguez@gmail.com>
2023-08-22 15:31:43 -0500

    added styles sheet to jsx file

--- cv-application/src/components/utilities/Button.jsx
+++ cv-application/src/components/utilities/Button.jsx
@@ -1,4 +1,6 @@
 /* eslint-disable react/prop-types */
+import "../../styles/Buttons.css";
+
 function Button({ className, type, text, onClick }) {
   return (
     <button className={className} type={type} onClick={onClick}>
