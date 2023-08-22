335266fd94c3b55c4078a60e69c0b4cfcbea4e9b
Selena <selena.k.rodriguez@gmail.com>
2023-08-22 12:05:46 -0500

    created a styles folder, moved the css files to it and updated their paths

--- cv-application/src/App.jsx
+++ cv-application/src/App.jsx
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./styles/App.css";
 import UserDetails from "./components/UserDetails";
 import ResumeDetails from "./components/ResumeDetails";
 import { useState } from "react";
