76c80c394afd39b167052af19c4269ea5d345366
Selena <selena.k.rodriguez@gmail.com>
2023-08-23 17:19:01 -0500

    add a horizontal line

--- cv-application/src/components/resume-display/SummaryDetails.jsx
+++ cv-application/src/components/resume-display/SummaryDetails.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import "../../styles/SummaryDetails.css";
 
 /* eslint-disable react/prop-types */
 function SummaryDetails({ summary }) {
@@ -15,6 +16,7 @@ function SummaryDetails({ summary }) {
   return (
     <>
       <h2 id="summary-title">Summary</h2>
+      <hr></hr>
       <h3 id="summary-text">{placeholder}</h3>
     </>
   );
